10a060c3ac78357de5c9ceda7b3b77b066fada0c
ColonelMelnyk <xiaomiredmia15@gmail.com>
2023-07-08 15:13:56 +0300

    pt1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
+import React, { Component } from 'react';
+
+ export class Feedback extends Component{
+
+state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+ 
+  handlePositiveFeedback = () =>{
+    this.setState(prevState =>({
+      good: prevState.good +1 
+    }));
+  };
+  handleNegativeFeedback = () =>{
+    this.setState(prevState =>({
+      bad: prevState.bad +1 
+    }));
+  };
+  handleNeutralFeedback =()=>{
+    this.setState(prevState =>({
+      neutral: prevState.neutral +1 
+    }));
+  };
+render(){
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h2>Please leave your feedback</h2>
+      <div>
+        <button onClick = {this.handlePositiveFeedback}>Good</button>
+        <button onClick = {this.handleNeutralFeedback}>Neutral</button>
+        <button onClick = {this.handleNegativeFeedback}>Bad</button>
+      </div>
+      <div>
+        <h2>Statistics</h2>
+        <span>Good: {this.state.good}</span><br />
+        <span>Neutral: {this.state.neutral}</span><br />
+        <span>Bad: {this.state.bad}</span><br />
+      </div>
     </div>
   );
-};
+}
+}

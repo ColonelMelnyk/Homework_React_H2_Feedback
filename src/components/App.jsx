54e3570226a0eaaaa2c8499f1d49c160d9c6babe
ColonelMelnyk <xiaomiredmia15@gmail.com>
2023-07-08 15:40:49 +0300

    pt2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,20 @@ state = {
       neutral: prevState.neutral +1 
     }));
   };
+  handleTotalFeedbackCount = () =>{
+    const {good, bad, neutral} = this.state;
+    const total = good + neutral + bad;
+    return total;
+  }
+  handlePositiveFeedbackPercentage = () => {
+    const { good, bad, neutral} = this.state;
+    const total = good + neutral + bad;
+    let percentage = (good / total)*100;
+    return Math.round(percentage) ; 
+  }
 render(){
+  const total = this.handleTotalFeedbackCount();
+  const percentage = this.handlePositiveFeedbackPercentage();
   return (
     <div>
       <h2>Please leave your feedback</h2>
@@ -37,6 +50,10 @@ render(){
         <span>Good: {this.state.good}</span><br />
         <span>Neutral: {this.state.neutral}</span><br />
         <span>Bad: {this.state.bad}</span><br />
+        <span>Total: {total}</span><br/>
+        {
+          percentage > 0 ? ( <span>Positive Feedback: {percentage}%</span>):(<span>Positive Feedback: 0%</span>)
+        }
       </div>
     </div>
   );
